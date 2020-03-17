fb6f161d935bb6e99a7a3150ccd40fcf95e39ca5
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-03-17 15:27:43 +0530

    Create PosNegZero.java

---
+++ Flow Control Statements/PosNegZero.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class PosNegZero {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = in.nextInt();
+
+        if(n > 0) System.out.println("Positive");
+        else if(n < 0) System.out.println("Negative");
+        else System.out.println("Zero");
+    }
+}
