11d2447d0af700ef40e86240b979ae848a06a339
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-03-17 16:25:23 +0530

    Create AlpDigCha.java

---
+++ Flow Control Statements/AlpDigCha.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+class AlpDigCha {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = in.nextLine().trim().charAt(0);
+
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            System.out.println("Alphabet");
+        else if (ch >= '0' && ch <= '9')
+            System.out.println("Digit");
+        else
+            System.out.println("Special Character");
+    }
+}
