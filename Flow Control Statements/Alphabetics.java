a20b0a5fd2e4fc335a534659cc493a096c5a1c64
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-03-17 15:40:09 +0530

    Create Alphabetics.java

---
+++ Flow Control Statements/Alphabetics.java
@@ -0,0 +1,11 @@
+class Alphabetics {
+    public static void main(String[] args) {
+        char first = 'a';
+        char second = 'r';
+
+        if(first < second)
+            System.out.println(first + ", " + second);
+        else
+            System.out.println(second + ", " + first);
+    }
+}
