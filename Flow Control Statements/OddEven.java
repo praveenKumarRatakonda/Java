4cd33e9a63dd299e420b6dd6b42a0e1ea5d839bb
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-03-17 15:30:21 +0530

    Create OddEven.java

---
+++ Flow Control Statements/OddEven.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class OddEven {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = in.nextInt();
+
+        if((n & 1) == 1) System.out.println("Odd");
+        else System.out.println("Even");
+    }
+}
