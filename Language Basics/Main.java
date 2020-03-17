80924fa03f8f1de69734949c212b8ec273bb5287
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-03-17 15:15:03 +0530

    Delete Main.java

--- Language Basics/Main.java
+++
@@ -1,8 +0,0 @@
-class Main{
-    public static void main(String[] args) {
-        if(args.length > 0)
-            System.out.println("Welcome " + args[0]);
-        else
-            System.out.println("Welcome");
-    }
-}
