c0952062860ffb54ab03304d9cfdfbe32b43913c
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-03-17 15:18:54 +0530

    Create WelcomeMessage.java

---
+++ Language Basics/WelcomeMessage.java
@@ -0,0 +1,8 @@
+class WelcomeMessage {
+    public static void main(String[] args) {
+        if(args.length > 0)
+            System.out.println("Welcome " + args[0]);
+        else
+            System.out.println("Welcome");
+    }
+}
