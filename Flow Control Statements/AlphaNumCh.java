6e36ce2d824e45e87f21fbe3def1f6859398f43e
praveenKumarRatakonda <57628139+praveenKumarRatakonda@users.noreply.github.com>
2020-03-17 16:10:46 +0530

    Create AlphaNumCh.java

---
+++ Flow Control Statements/AlphaNumCh.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class AlphNumCh{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = in.nextLine().trim().charAt(0);
+
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+            System.out.println("Alphabet");
+        else if (ch >= '0' && ch <= '9')
+            System.out.println("Digit");
+        else
+            System.out.println("Special Character");
+    }
+}
